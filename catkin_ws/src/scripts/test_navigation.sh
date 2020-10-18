2c57c9e2fe6f7b0da8fdb6808c3ff21c57a7abba
Cristina Villegas <cristina.villegas@gmail.com>
2020-10-18 06:07:17 +0000

    add test_navigation script to launch for navigation

---
+++ catkin_ws/src/scripts/test_navigation.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+export ROBOT_INITIAL_POSE="-x 4.5 -y -6.5 -Y 0"
+
+xterm -e "source /home/workspace/udacity_home_service_robot/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/udacity_home_service_robot/catkin_ws/src/my_robot/worlds/apartment" &
+
+sleep 15
+
+xterm -e "source /home/workspace/udacity_home_service_robot/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/udacity_home_service_robot/catkin_ws/src/my_robot/maps/apartment.yaml initial_pose_x:=-6.5 initial_pose_y:=-4.5 initial_pose_a:=-1.57" &
+
+sleep 2
+
+xterm -e "source /home/workspace/udacity_home_service_robot/catkin_ws/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
