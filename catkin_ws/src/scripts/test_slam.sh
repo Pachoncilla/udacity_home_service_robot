7b63c0edcc2bc1bb52cb2b3e0b6dee17eb2bb740
Cristina Villegas <cristina.villegas@gmail.com>
2020-10-18 06:06:41 +0000

    add initial position inside map

--- catkin_ws/src/scripts/test_slam.sh
+++ catkin_ws/src/scripts/test_slam.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+export ROBOT_INITIAL_POSE="-x 4.5 -y -6.5 -Y 0"
+
 xterm -e "source /home/workspace/udacity_home_service_robot/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/udacity_home_service_robot/catkin_ws/src/my_robot/worlds/apartment" &
 
 sleep 15
