48c73042d5e71d91f1037f17889614fd74493aab
Cristina Villegas <cristina.villegas@gmail.com>
2020-10-18 04:47:19 +0000

    add launch file to perform slam

---
+++ catkin_ws/src/scripts/test_slam.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+xterm -e "source /home/workspace/udacity_home_service_robot/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
+
+sleep 5
+
+xterm -e "source /home/workspace/udacity_home_service_robot/catkin_ws/devel/setup.bash; roslaunch gmapping slam_gmapping_pr2.launch" &
+
+sleep 2
+
+xterm -e "source /home/workspace/udacity_home_service_robot/catkin_ws/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+
+sleep 2
+
+xterm -e "source /home/workspace/udacity_home_service_robot/catkin_ws/devel/setup.bash; roslaunch turtlebot_teleop keyboard_teleop.launch"
