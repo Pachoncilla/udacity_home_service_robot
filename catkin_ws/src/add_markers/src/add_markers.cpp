44a33560e24a3a023f6113140dbf3d7e5d0368c1
Pachoncilla <cristina.villegas@gmail.com>
2020-10-18 19:51:03 +0200

    add two markes on the pick-up location

--- catkin_ws/src/add_markers/src/add_markers.cpp
+++ catkin_ws/src/add_markers/src/add_markers.cpp
@@ -30,6 +30,19 @@
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
 
+
+void SetMarkerPosition(visualization_msgs::Marker& marker, float x, float y,
+                      float
+                                                                   orientation){
+  // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
+  marker.pose.position.x = x;
+  marker.pose.position.y = y;
+  marker.pose.position.z = 0;
+  marker.pose.orientation.x = 0.0;
+  marker.pose.orientation.y = 0.0;
+  marker.pose.orientation.z = 0.0;
+  marker.pose.orientation.w = orientation;
+}
 int main( int argc, char** argv )
 {
   ros::init(argc, argv, "add_markers");
@@ -38,7 +51,7 @@ int main( int argc, char** argv )
   ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
 
   // Set our initial shape type to be a cube
-  uint32_t shape = visualization_msgs::Marker::CUBE;
+  uint32_t shape_cube = visualization_msgs::Marker::CUBE;
 
   while (ros::ok())
   {
@@ -54,19 +67,11 @@ int main( int argc, char** argv )
     marker.id = 0;
 
     // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
-    marker.type = shape;
+    marker.type = shape_cube;
 
     // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
     marker.action = visualization_msgs::Marker::ADD;
 
-    // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
-    marker.pose.position.x = 0;
-    marker.pose.position.y = 0;
-    marker.pose.position.z = 0;
-    marker.pose.orientation.x = 0.0;
-    marker.pose.orientation.y = 0.0;
-    marker.pose.orientation.z = 0.0;
-    marker.pose.orientation.w = 1.0;
 
     // Set the scale of the marker -- 1x1x1 here means 1m on a side
     marker.scale.x = 1.0;
@@ -81,6 +86,8 @@ int main( int argc, char** argv )
 
     marker.lifetime = ros::Duration();
 
+
+    SetMarkerPosition(marker, -2.0, -6.0, 1.0);
     // Publish the marker
     while (marker_pub.getNumSubscribers() < 1)
     {
@@ -93,22 +100,33 @@ int main( int argc, char** argv )
     }
     marker_pub.publish(marker);
 
-    // Cycle between different shapes
-    switch (shape)
+    sleep(5);
+    marker.action = visualization_msgs::Marker::DELETE;
+    while (marker_pub.getNumSubscribers() < 1)
     {
-    case visualization_msgs::Marker::CUBE:
-      shape = visualization_msgs::Marker::SPHERE;
-      break;
-    case visualization_msgs::Marker::SPHERE:
-      shape = visualization_msgs::Marker::ARROW;
-      break;
-    case visualization_msgs::Marker::ARROW:
-      shape = visualization_msgs::Marker::CYLINDER;
-      break;
-    case visualization_msgs::Marker::CYLINDER:
-      shape = visualization_msgs::Marker::CUBE;
-      break;
+      if (!ros::ok())
+      {
+        return 0;
+      }
+      ROS_WARN_ONCE("Please create a subscriber to the marker");
+      sleep(1);
     }
+    marker_pub.publish(marker);
+
+    sleep(5);
+
+    marker.action = visualization_msgs::Marker::ADD;
+    SetMarkerPosition(marker, -3.5, -7.5, 1.0);
+    while (marker_pub.getNumSubscribers() < 1)
+    {
+      if (!ros::ok())
+      {
+        return 0;
+      }
+      ROS_WARN_ONCE("Please create a subscriber to the marker");
+      sleep(1);
+    }
+    marker_pub.publish(marker);
     r.sleep();
   }
 }
