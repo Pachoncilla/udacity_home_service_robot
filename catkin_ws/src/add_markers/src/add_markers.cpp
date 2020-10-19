3ef0172e4cfba09064f5c0882b9638403d91dac2
Pachoncilla <cristina.villegas@gmail.com>
2020-10-19 17:32:57 +0200

    use message pick_objects in add_markers

--- catkin_ws/src/add_markers/src/add_markers.cpp
+++ catkin_ws/src/add_markers/src/add_markers.cpp
@@ -5,40 +5,40 @@
 
 #include <ros/ros.h>
 #include "manage_markers.h"
+#include <pick_objects/pick_objects.h>
 
+auto marker = CreateMarker();
+ros::Publisher marker_pub;
+
+void pick_callback(const pick_objects::pick_objects& msg){
+
+  while (marker_pub.getNumSubscribers() < 1)
+  {
+    ROS_WARN_ONCE("Please create a subscriber to the marker");
+    sleep(1);
+  }
+
+  if (msg.action == pick_objects::pick_objects::PICKING_OBJECT ||
+      msg.action == pick_objects::pick_objects::OBJECT_DELIVERED) {
+    AddMarker(marker);
+    SetMarkerPosition(marker, msg.x, msg.y, msg.orientation);
+    marker_pub.publish(marker);
+    return;
+  }
+  if (msg.action == pick_objects::pick_objects::DELIVERING_OBJECT) {
+    RemoveMarker(marker);
+    marker_pub.publish(marker);
+    return;
+  }
+
+}
 int main( int argc, char** argv )
 {
   ros::init(argc, argv, "add_markers");
   ros::NodeHandle n;
   ros::Rate r(1);
-  ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+  marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
 
-  while (ros::ok())
-  {
-    while (marker_pub.getNumSubscribers() < 1)
-    {
-      if (!ros::ok())
-      {
-        return 0;
-      }
-      ROS_WARN_ONCE("Please create a subscriber to the marker");
-      sleep(1);
-    }
-
-    auto marker = CreateMarker();
-    SetMarkerPosition(marker, -2.0, -6.0, 1.0);
-    marker_pub.publish(marker);
-    sleep(5);
-    
-    RemoveMarker(marker);
-    marker_pub.publish(marker);
-    sleep(5);
-
-    AddMarker(marker);
-    SetMarkerPosition(marker, -3.5, -7.5, 1.0);
-    marker_pub.publish(marker);
-    sleep(5);
-
-    r.sleep();
-  }
+  ros::Subscriber pick_objects_sub = n.subscribe("pick_objects", 1000,
+                                                 pick_callback);
 }
