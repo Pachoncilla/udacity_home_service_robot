7922e51df84a0321b1bf91f2b914b3f81510a03b
Pachoncilla <cristina.villegas@gmail.com>
2020-10-21 21:03:03 +0200

    pick objects publishes info of status of picking action

--- catkin_ws/src/pick_objects/src/pick_objects.cpp
+++ catkin_ws/src/pick_objects/src/pick_objects.cpp
@@ -1,7 +1,9 @@
 #include <ros/ros.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
+#include <pick_objects/pick_objects.h>
 
+ros::Publisher marker_pub;
 // Define a client for to send goal requests to the move_base server through a SimpleActionClient
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
@@ -20,9 +22,37 @@ move_base_msgs::MoveBaseGoal createGoal(float x, float y, float orientation){
   return goal;
 }
 
+void publishPikingObject(float x, float y, float orientation){
+  pick_objects::pick_objects marker;
+  marker.x = x;
+  marker.y = y;
+  marker.orientation = orientation;
+  marker.action = pick_objects::pick_objects::PICKING_OBJECT;
+  marker_pub.publish(marker);
+}
+
+void publishObjectDelivered(float x, float y, float orientation){
+  pick_objects::pick_objects marker;
+  marker.x = x;
+  marker.y = y;
+  marker.orientation = orientation;
+  marker.action = pick_objects::pick_objects::OBJECT_DELIVERED;
+  marker_pub.publish(marker);
+}
+
+void publishDeliveringObject(){
+  pick_objects::pick_objects marker;
+  marker.action = pick_objects::pick_objects::DELIVERING_OBJECT;
+  marker_pub.publish(marker);
+}
+
 int main(int argc, char** argv){
   // Initialize the simple_navigation_goals node
   ros::init(argc, argv, "pick_objects");
+  ros::NodeHandle n;
+  marker_pub = n.advertise<pick_objects::pick_objects>
+      ("pick_objects", 1);
+
 
   //tell the action client that we want to spin a thread by default
   MoveBaseClient ac("move_base", true);
@@ -36,6 +66,7 @@ int main(int argc, char** argv){
   // Send the goal position and orientation for the robot to reach
   ROS_INFO("Sending goal");
   ac.sendGoal(createGoal(-2.0, -6.0, 1.0));
+  publishPikingObject(-2.0, -6.0, 1.0);
 
   // Wait an infinite time for the results
   ac.waitForResult();
@@ -43,7 +74,7 @@ int main(int argc, char** argv){
   // Check if the robot reached its goal
   if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
     ROS_INFO("Hooray, the base moved to goal position and is picking up the item");
-
+    publishDeliveringObject();
   } else {
     ROS_INFO("The base failed to move to the goal position for some reason");
     return 0;
@@ -52,6 +83,7 @@ int main(int argc, char** argv){
   sleep(5.0);
 
   ac.sendGoal(createGoal(-3.5, -7.5, 1.0));
+  publishObjectDelivered(-3.5, -7.5, 1.0);
   ac.waitForResult();
   if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
     ROS_INFO("Hooray, the base moved to the goal position to deliver the item");
